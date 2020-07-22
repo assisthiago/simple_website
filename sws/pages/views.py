35cbec7b45432dcbfd4246f98f689dcd36e42b54
Thiago Assis <thiago.medassis@gmail.com>
2020-07-22 20:31:44 -0300

    Filtros

--- sws/pages/views.py
+++ sws/pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def listings(request):
+    return render(request, 'pages/listings.html')
