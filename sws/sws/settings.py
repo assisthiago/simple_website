f77c26a319373cf1b8ee22f9385950d4cdf2c4c1
Thiago Assis <thiago.medassis@gmail.com>
2020-07-27 20:51:39 -0300

    local_settings

--- sws/sws/settings.py
+++ sws/sws/settings.py
@@ -128,3 +128,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sws/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
